8bb4030410c7209761a3a771fe56a629dc258dd2
Vladimir Lapchenko <vlr.lapchenko@gmail.com>
2017-06-21 18:02:25 +0300

    issue list

--- Source/Zhaba.DBAccess/SQL/Filters/IssueList.cs
+++ Source/Zhaba.DBAccess/SQL/Filters/IssueList.cs
@@ -32,47 +32,6 @@ namespace Zhaba.DBAccess.SQL.Filters
         cmd.Parameters.AddWithValue("pName", name);
       }
 
-      var description = filter.Description.ParseName();
-      if (description != null)
-      {
-        where += "AND (TI.DESCRIPTION LIKE ?pDescription)";
-        cmd.Parameters.AddWithValue("pDescription", description);
-      }
-
-      var milestone = filter.Milestone.ParseName();
-      if (milestone != null)
-      {
-        where += "AND (TM.NAME LIKE ?pMilestoneName)";
-        cmd.Parameters.AddWithValue("pMilestoneName", milestone);
-      }
-
-      var area = filter.Area.ParseName();
-      if (area != null)
-      {
-        where += "AND (TA.NAME LIKE ?pAreaName)";
-        cmd.Parameters.AddWithValue("pAreaName", area);
-      }
-
-      var component = filter.Component.ParseName();
-      if (component != null)
-      {
-        where += "AND (TC.NAME LIKE ?pComponentName)";
-        cmd.Parameters.AddWithValue("pComponentName", component);
-      }
-
-      List<Tuple<DateTime?, DateTime?>> dates;
-      if (filter.CreationDateSpan.ParseDateSpan(out dates) && dates != null)
-      {
-        var sb = BuildDatesFilter("TI.CREATION_DATE", dates, cmd);
-        where += "AND ({0}) \r\n".Args(sb);
-      }
-
-      if (filter.ChangeDateSpan.ParseDateSpan(out dates) && dates != null)
-      {
-        var sb = BuildDatesFilter("TI.CHANGE_DATE", dates, cmd);
-        where += "AND ({0}) \r\n".Args(sb);
-      }
-
       string order = "TI.Counter";
       if (filter.OrderBy.IsNotNullOrWhiteSpace())
       {
@@ -87,24 +46,9 @@ namespace Zhaba.DBAccess.SQL.Filters
       cmd.CommandText =
 @"SELECT
   TI.COUNTER,
-	TI.NAME,
-	TI.DESCRIPTION,
-	TI.C_MILESTONE,
-	TI.C_AREA,
-	TI.C_COMPONENT,
-	TI.STATUS,
-	TI.CREATOR,
-	TI.OWNER,
-	TI.CREATION_DATE,
-	TI.CHANGE_DATE,
-  TM.NAME AS MILESTONE_NAME,
-  TA.NAME AS AREA_NAME,
-  TC.NAME AS COMPONENT_NAME
+  TI.NAME
 FROM
-  tbl_issue     TI LEFT OUTER JOIN
-  tbl_milestone TM ON (TI.C_MILESTONE = TM.COUNTER) LEFT OUTER JOIN
-  tbl_area      TA ON (TI.C_AREA = TA.COUNTER) LEFT OUTER JOIN
-  tbl_component TC ON (TI.C_COMPONENT = TC.COUNTER)
+  tbl_issue TI
 WHERE
   (TI.C_PROJECT = ?pProj_ID)
   {0}
