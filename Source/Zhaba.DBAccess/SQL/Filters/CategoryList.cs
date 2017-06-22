28e4ed1257fa830b3418fcd1427645e87df3ddb8
eibraev <eibraev123>
2017-06-22 19:30:15 +0600

    fix

---
+++ Source/Zhaba.DBAccess/SQL/Filters/CategoryList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using NFX.DataAccess.MySQL;
+using Zhaba.Data.Filters;
+using Zhaba.DBAccess.Handlers;
+using NFX;
+
+namespace Zhaba.DBAccess.SQL.Filters
+{
+    public class CategoryList : ZhabaFilteredMySQLQueryHandler<CategoryListFilter>
+    {
+        public CategoryList(MySQLDataStore store, string name) : base(store, name)
+        {
+        }
+
+        protected override void DoBuildCommandAndParms(MySQLCRUDQueryExecutionContext context, MySqlCommand cmd, CategoryListFilter filter)
+        {
+            string where = string.Empty;
+
+            var name = filter.Name.ParseName();
+            if (name != null)
+            {
+                where += "AND (TP.NAME LIKE ?pName)";
+                cmd.Parameters.AddWithValue("pName", name);
+            }
+
+            var description = filter.Description.ParseName();
+            if (description != null)
+            {
+                where += "AND (TP.DESCRIPTION LIKE ?pDescription)";
+                cmd.Parameters.AddWithValue("pDescription", description);
+            }
+
+            string order = "TP.Counter";
+            if (filter.OrderBy.IsNotNullOrWhiteSpace())
+            {
+                var desc = filter.OrderBy.StartsWith("-");
+                if (desc)
+                    order = "TP." + filter.OrderBy.Substring(1) + " DESC";
+                else
+                    order = "TP." + filter.OrderBy + " ASC";
+            }
+
+            cmd.CommandText =
+      @"SELECT *
+FROM tbl_category TP
+WHERE (IN_USE = 'T') {0}
+ORDER BY {1}".Args(where, order);
+        }
+    }
+}
