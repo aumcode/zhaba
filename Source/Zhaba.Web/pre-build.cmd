e9b24846e42f9a82c1ecc58e7b27b46f73c009a5
Harkonnen <ordos13@yandex.ru>
2017-06-29 13:42:44 +0300

    Change pre-build script

--- Source/Zhaba.Web/pre-build.cmd
+++ Source/Zhaba.Web/pre-build.cmd
@@ -3,9 +3,9 @@ set PROJECT_DIR=%~dp0
 
 echo %PROJECT_DIR%
 
-"%SOLUTION_DIR%packages\NFX.3.4.0.1\tools\ntc" "%PROJECT_DIR%Pages\*.nht" -r -ext ".auto.cs" -src -c "NFX.Templatization.NHTCompiler, NFX"
-"%SOLUTION_DIR%packages\NFX.3.4.0.1\tools\ntc" "%PROJECT_DIR%Pages\__Masters\*.nht" -r -ext ".auto.cs" -src -c "NFX.Templatization.NHTCompiler, NFX"
-"%SOLUTION_DIR%packages\NFX.3.4.0.1\tools\ntc" "%PROJECT_DIR%Pages\List\*.nht" -r -ext ".auto.cs" -src -c "NFX.Templatization.NHTCompiler, NFX"
-"%SOLUTION_DIR%packages\NFX.3.4.0.1\tools\ntc" "%PROJECT_DIR%Pages\Edit\*.nht" -r -ext ".auto.cs" -src -c "NFX.Templatization.NHTCompiler, NFX"
-"%SOLUTION_DIR%packages\NFX.3.4.0.1\tools\ntc" "%PROJECT_DIR%Controls\*.nht" -r -ext ".auto.cs" -src -c "NFX.Templatization.NHTCompiler, NFX"
+"%SOLUTION_DIR%packages\NFX.3.5.0.0\tools\ntc" "%PROJECT_DIR%Pages\*.nht" -r -ext ".auto.cs" -src -c "NFX.Templatization.NHTCompiler, NFX"
+"%SOLUTION_DIR%packages\NFX.3.5.0.0\tools\ntc" "%PROJECT_DIR%Pages\__Masters\*.nht" -r -ext ".auto.cs" -src -c "NFX.Templatization.NHTCompiler, NFX"
+"%SOLUTION_DIR%packages\NFX.3.5.0.0\tools\ntc" "%PROJECT_DIR%Pages\List\*.nht" -r -ext ".auto.cs" -src -c "NFX.Templatization.NHTCompiler, NFX"
+"%SOLUTION_DIR%packages\NFX.3.5.0.0\tools\ntc" "%PROJECT_DIR%Pages\Edit\*.nht" -r -ext ".auto.cs" -src -c "NFX.Templatization.NHTCompiler, NFX"
+"%SOLUTION_DIR%packages\NFX.3.5.0.0\tools\ntc" "%PROJECT_DIR%Controls\*.nht" -r -ext ".auto.cs" -src -c "NFX.Templatization.NHTCompiler, NFX"
 call lessc "%PROJECT_DIR%Static\less\zhb.less" "%PROJECT_DIR%Static\css\zhb.css"
