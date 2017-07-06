4085574881364e7ba10ef3325df520aa15433d1c
eibraev <eibraev123>
2017-07-06 16:46:31 +0600

    Edit progress in PopUpDialog Click on protress

--- Source/Zhaba.DataLogic/IssueLog/ZhabaIssueLogLogic.cs
+++ Source/Zhaba.DataLogic/IssueLog/ZhabaIssueLogLogic.cs
@@ -355,7 +355,7 @@ namespace Zhaba.DataLogic
     {
       operations = operations ?? ZApp.Data.CRUD;
       IssueLogRow newRow = NewIssueLog(evt, operations:operations);
-      if (newRow.Status == ZhabaIssueStatus.CLOSED) return;
+      if (ZhabaIssueStatus.CLOSED.EqualsOrdIgnoreCase(newRow.Status) || ZhabaIssueStatus.CANCELED.EqualsOrdIgnoreCase(newRow.Status)) return;
       newRow.Completeness = evt.Completeness;
       newRow.Description = evt.Description;
 //      if (evt.Completeness == 100) newRow.Status = ZhabaIssueStatus.DONE;
